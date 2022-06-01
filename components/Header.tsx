8904646412614d6a41cffcba8ad67b9591d8ca85
luisberzoza <73074337+luisberzoza@users.noreply.github.com>
2022-06-01 00:27:30 -0500

    starting header

---
+++ components/Header.tsx
@@ -0,0 +1,17 @@
+
+function Header() {
+    return (
+        <header>
+            <div>
+                <div>
+                    <div className="font-Cedarville">Luis Berzoza</div>
+                </div>
+            </div>
+
+            <div></div>
+        </header>
+    )
+}
+
+export
+ default Header
