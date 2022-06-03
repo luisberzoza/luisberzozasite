200caaa7d17fca83a2490e3855dad6538728a30f
luisberzoza <73074337+luisberzoza@users.noreply.github.com>
2022-06-03 01:07:04 -0500

    header 2

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,25 @@
 
 function Header() {
     return (
-        <header>
-            <div>
-                <div>
-                    <div className="font-Cedarville">Luis Berzoza</div>
-                </div>
-            </div>
-            <h1 className="">This is the HEADER</h1>
-            <div>
+        <header className="">
+            <div className="flex">
+                <div className="flex items-center space-x-5">
+                    <div className="hidden md:inline-flex object-left">
+                        <h3>Portfolio</h3>
+                        <h3>About Me</h3>
+                        <h3>Let's Talk</h3>
+                    </div>
 
+                    <div className="font-Cedarville w-44 object-contain cursor-pointer md:object-top md:text-center">Luis Berzoza</div>      
+
+                    <div className="hidden md:inline-flex object-right">
+                        <h3>Facebook</h3>
+                        <h3>Twitter</h3>
+                        <h3>Linkedin</h3>
+                    </div>
+                </div>    
+            </div>         
+            <div>
             </div>
         </header>
     )
