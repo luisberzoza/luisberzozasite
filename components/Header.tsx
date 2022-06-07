9f40c7985f7fa6b0586fbc0dad0d6b131a814fe4
luisberzoza <73074337+luisberzoza@users.noreply.github.com>
2022-06-07 00:42:05 -0500

    header icons p1

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,26 +1,23 @@
+// import FacebookIcon from '@mui/icons-material/Facebook';
 
 function Header() {
     return (
         <header className="">
-            <div className="flex">
-                <div className="flex items-center space-x-5">
-                    <div className="hidden md:inline-flex object-left">
+                <div className="flex items-center space-x-5 md:justify-evenly">
+                    <div className="hidden md:inline-flex object-left space-x-5">
                         <h3>Portfolio</h3>
                         <h3>About Me</h3>
                         <h3>Let's Talk</h3>
                     </div>
 
-                    <div className="font-Cedarville w-44 object-contain cursor-pointer md:object-top md:text-center">Luis Berzoza</div>      
+                    <div className="font-Cedarville text-2xl w-44 object-contain cursor-pointer">Luis Berzoza</div>      
 
-                    <div className="hidden md:inline-flex object-right">
-                        <h3>Facebook</h3>
+                    <div className="hidden md:inline-flex object-right content-end space-x-5">
+                        <a > <Facebook /> </a>
                         <h3>Twitter</h3>
                         <h3>Linkedin</h3>
                     </div>
-                </div>    
-            </div>         
-            <div>
-            </div>
+                </div>
         </header>
     )
 }
