562beb5d3cf47cdf327f4520d55ce0aa5b9036cb
luisberzoza <73074337+luisberzoza@users.noreply.github.com>
2022-06-20 00:57:04 -0500

    hamburger menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,24 +1,27 @@
+import React, { useState } from "react";
 import FacebookIcon from '@mui/icons-material/Facebook';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function Header() {
     return (
-        <header className="">
-                <div className="flex items-center space-x-5 object-left md:justify-evenly">
-                    <div className="hidden md:inline-flex object-left space-x-5">
-                        <h3>Portfolio</h3>
-                        <h3>About Me</h3>
-                        <h3>Let's Talk</h3>
-                    </div>
+        <header className="flex justify-between mx-auto p-8 max-w-7xl ease-in">
+                <div className="hidden md:inline-flex space-x-2 items-center ">
+                    <h3>Portfolio</h3>
+                    <h3>About Me</h3>
+                    <h3>Contact</h3>
+                </div>
 
-                    <div className="font-Cedarville text-2xl content-center w-44 object-contain cursor-pointer">Luis Berzoza</div>      
+                <a href="/" className="font-Cedarville text-3xl object-left md:place-content-center cursor-pointer">Luis Berzoza</a>      
 
-                    <div className="hidden md:inline-flex object-right content-end space-x-5">
-                        <a href="" className="cursor-pointer"> <FacebookIcon/> </a>
-                        <a href="" className="cursor-pointer"><TwitterIcon/></a>
-                        <a href="" className="cursor-pointer"> <LinkedInIcon/></a>
-                    </div>
+                <div className="hidden md:inline-flex space-x-5 items-center ">
+                    <a href="" className="cursor-pointer"> <FacebookIcon /> </a>
+                    <a href="" className="cursor-pointer"><TwitterIcon /></a>
+                    <a href="" className="cursor-pointer"> <LinkedInIcon /></a>
+                </div>
+                <div className="ml-5 flex items-center lg:hidden md:hidden cursor-pointer">
+                    <MenuIcon />
                 </div>
         </header>
     )
